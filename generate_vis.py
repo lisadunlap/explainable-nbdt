ab794f1856b06cf0c51dd7f81b652180393a23d5
arilato <arilato>
2020-03-18 20:22:25 -0700

    added some multiple path support

--- generate_vis.py
+++ generate_vis.py
@@ -37,14 +37,14 @@ def build_graph(G):
     }
 
 
-def generate_vis(path_template, data, name, fname):
+def generate_vis(path_template, data, name, fname, out_dir='out/'):
     with open(path_template) as f:
         html = f.read().replace(
             "'TREE_DATA_CONSTANT_TO_BE_REPLACED'",
             json.dumps(data))
 
     os.makedirs('out', exist_ok=True)
-    path_html = f'out/{fname}-{name}.html'
+    path_html = f'{out_dir}{fname}-{name}.html'
     with open(path_html, 'w') as f:
         f.write(html)
 
@@ -62,11 +62,11 @@ def main():
         for f in os.listdir(path):
             if f.split('.')[-1] != 'json':
                 continue
-            setup_vis(args, os.path.join(path, f))
+            setup_vis(args, os.path.join(path, f), out_dir=f'out/{path.split("/")[-2]}/')
     else:
         setup_vis(args, path)
 
-def setup_vis(args, path):
+def setup_vis(args, path, out_dir='out/'):
     print('==> Reading from {}'.format(path))
 
     G = read_graph(path)
@@ -83,9 +83,10 @@ def setup_vis(args, path):
         print(f'Found just {num_roots} root.')
 
     #fname = generate_fname(**vars(args)).replace('graph-', '', 1)
-    fname = path.split('/')[-1].replace('.pth', '')
+    fname = path.split('/')[-1].replace('.pth', '').replace('.json', '')
+    print(fname)
     if args.mode == 'weighted':
-        generate_vis('vis/tree-weighted-template.html', tree, 'tree', fname)
+        generate_vis('vis/tree-weighted-template.html', tree, 'tree', fname, out_dir=out_dir)
     else:
         generate_vis('vis/tree-template.html', tree, 'tree', fname)
         generate_vis('vis/graph-template.html', graph, 'graph', fname)
