70105191643c7d51f2b90d489e1ff0877937f34e
Lisa Dunlap <lisabdunlap@berkeley.edu>
2020-03-19 17:50:21 -0700

    added ability to modfy paths

---
+++ modify_tree.py
@@ -0,0 +1,35 @@
+from nbdt.utils import DATASETS, METHODS, DATASET_TO_FOLDER_NAME, Colors
+from nbdt.graph import get_parser, get_wnids_from_dataset, read_graph, \
+    get_leaves, generate_fname, get_directory, get_graph_path_from_args, \
+    get_roots, add_paths, write_graph, augment_graph, condense_leaves
+from pathlib import Path
+import argparse
+import os
+
+def main():
+
+    parser = get_parser()
+    args = parser.parse_args()
+
+    wnids = get_wnids_from_dataset(args.dataset)
+    path = get_graph_path_from_args(args)
+    if args.json_path:
+        path = args.json_path
+    print('==> Reading from {}'.format(path))
+
+    G = read_graph(path)
+
+    if args.method == 'clustered':
+        G = condense_leaves(G)
+        write_path = path.replace('.json', '-clustered')
+        write_graph(G, write_path)
+
+    if args.parents:
+        G = add_paths(G, args.parents, args.children)
+        write_path = path.replace('.json', '-modified')
+        write_graph(G, write_path)
+
+    Colors.green('==> Wrote tree to {}'.format(write_path))
+
+if __name__ == '__main__':
+    main()
