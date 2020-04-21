780117f2074a85f46bc0e2271433a6b623d822b7
arilato <arilato>
2020-04-21 01:31:04 -0700

    added functionality for attributes loading

--- nbdt/data/custom.py
+++ nbdt/data/custom.py
@@ -709,36 +709,55 @@ class MiniPlaces(Dataset):
 
 
 class AnimalsWithAttributes2(Dataset): 
-    def __init__(self, root, transform, train=True, download=False, shuffle=False, **kwargs):
+    def __init__(self, root, transform, train=True, download=False, shuffle=False, binary=True, **kwargs):
         # load in the data, we ignore test case, only train/val
         self.root = os.path.join(root, 'awa2')
         labels_fname = 'trainclasses.txt' if train else 'testclasses.txt'
-        photos_path = os.path.join(self.root, 'JPEGImages')
-        labels_path = os.path.join(self.root, labels_fname)
+        matrix_fname = 'predicate-matrix-binary.txt' if binary else 'predicate-matrix-continuous.txt'
+
+        self.photos_path = os.path.join(self.root, 'JPEGImages')
+        self.labels_path = os.path.join(self.root, labels_fname)
+        self.classes_path = os.path.join(self.root, 'classes.txt')
+        self.predicates_path = os.path.join(self.root, 'predicates.txt')
+        self.predicates_matrix_path = os.path.join(self.root, matrix_fname)
 
         self.shuffle = shuffle
-        self.photos_path = photos_path
-        self.labels_path = labels_path
         self.transform = transform
         self.load_size = 224
         self.images = []
         self.labels = []
         self.classes = []
+        self.use_classes = []
+        self.predicates = []
+        self.class_predicates = []
 
         if download:
             # TODO check if file already exists, otherwise download
             pass
 
+        with open(self.classes_path, 'r') as f:
+            for i, line in enumerate(f):
+                _, cls = line.strip().split('\t')
+                self.classes.append(cls)
 
-        # read the file
+        with open(self.predicates_path, 'r') as f:
+            for i, line in enumerate(f):
+                _, predicate = line.strip().split('\t')
+                self.predicates.append(predicate)
+
+        with open(self.predicates_matrix_path, 'r') as f:
+            for i, line in enumerate(f):
+                predicates = line.strip().split(' ')
+                self.class_predicates.append(predicates)
+
         # assuming that file hierarchy is {train/val}/{first letter}/{class}/{fname}.xml
         with open(self.labels_path, 'r') as f: 
             for i, line in enumerate(f):
                 label = line.strip()
-                self.classes.append(label)
+                self.use_classes.append(label)
 
         for image_folder in os.listdir(self.photos_path):
-            if image_folder in self.classes:
+            if image_folder in self.use_classes:
                 label = self.classes.index(image_folder)
                 image_folder_path = os.path.join(self.photos_path, image_folder)
                 new_image_paths = [os.path.join(image_folder_path, f) for f in os.listdir(image_folder_path)]
@@ -759,15 +778,18 @@ class AnimalsWithAttributes2(Dataset):
         with open(os.path.join(root, 'fake_wnid_to_class.json'), 'w') as f:
             json.dump(wnid_to_class, f)
 
+        print(self.__getitem__(0))
+
     def __len__(self): 
         return len(self.images)
 
     def __getitem__(self, idx): 
-        image = Image.open(os.path.join(self.photos_path, self.images[idx]))
+        image = Image.open(self.images[idx])
         image = self.transform(image)
         # label is the index of the correct category
         label = self.labels[idx]
-        return (image, label)
+        predicates = self.class_predicates[self.classes.index(self.use_classes[label])]
+        return (image, label, predicates)
 
     def setup_custom_wnids(self, root):
         wnid_to_class = {}
