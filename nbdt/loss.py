04386e94a0dde08c3bcf027878736d45287351b3
Lisa Dunlap <lisabdunlap@berkeley.edu>
2020-05-15 09:49:36 -0700

    added label smoothing

--- nbdt/loss.py
+++ nbdt/loss.py
@@ -8,7 +8,7 @@ from nbdt.utils import Colors, MaskLoss
 from saliency.Grad_CAM.gcam_loss import GradCAM
 
 __all__ = names = ('HardTreeSupLoss', 'SoftTreeSupLoss', 'CrossEntropyLoss', 'HardTreeSupLossMultiPath',
-                   'AttentionLoss', 'HardAttentionLoss', 'SoftTreeSupMaskLoss')
+                   'HardAttentionLoss', 'SoftTreeSupMaskLoss', 'ZSSoftTreeSupLoss')
 keys = (
     'path_graph', 'path_wnids', 'max_leaves_supervised',
     'min_leaves_supervised', 'weighted_average', 'tree_supervision_weight',
@@ -67,7 +67,6 @@ class TreeSupLoss(nn.Module):
         self.criterion = criterion
         self.tree_criterion = nn.BCEWithLogitsLoss()
 
-
 class HardTreeSupLoss(TreeSupLoss):
 
     def forward(self, outputs, targets):
@@ -75,7 +74,6 @@ class HardTreeSupLoss(TreeSupLoss):
         The supplementary losses are all uniformly down-weighted so that on
         average, each sample incurs half of its loss from standard cross entropy
         and half of its loss from all nodes.
-
         The code below is structured weirdly to minimize number of tensors
         constructed and moved from CPU to GPU or vice versa. In short,
         all outputs and targets for nodes with 2 children are gathered and
@@ -116,24 +114,19 @@ class HardTreeSupLoss(TreeSupLoss):
         return loss
 
     @classmethod
-    def inference(cls, node, outputs, targets, weighted_average=False, ignore_classes=[]):
-        # which classes to ignore? the ones in ignore_classes that are not direct leaves of our node
-        ignore_classes_pruned = node.prune_ignore_labels(ignore_classes)
-
-        classes = [node.old_to_new_classes[int(t)] if int(t) in ignore_classes_pruned else [] for t in targets]
+    def inference(cls, node, outputs, targets, weighted_average=False):
+        classes = [node.old_to_new_classes[int(t)] for t in targets]
         selector = [bool(cls) for cls in classes]
         targets_sub = [cls[0] for cls in classes if cls]
 
         _outputs = outputs[selector]
         if _outputs.size(0) == 0:
             return selector, _outputs[:, :node.num_classes], targets_sub
-
-        outputs_sub = cls.get_output_sub(_outputs, node, weighted_average, ignore_classes_pruned)
-
+        outputs_sub = cls.get_output_sub(_outputs, node, weighted_average)
         return selector, outputs_sub, targets_sub
 
     @staticmethod
-    def get_output_sub(_outputs, node, weighted_average=False, ignore_classes=[]):
+    def get_output_sub(_outputs, node, weighted_average=False):
         if weighted_average:
             node.move_leaf_weights_to(_outputs.device)
 
@@ -141,20 +134,11 @@ class HardTreeSupLoss(TreeSupLoss):
             node.new_to_leaf_weights[new_label] if weighted_average else 1
             for new_label in range(node.num_classes)
         ]
-
-        cls_idxs = [node.new_to_old_classes[new_label] for new_label in range(node.num_classes)]
-
-        for cls in ignore_classes:
-            for cls_idx in cls_idxs:
-                if cls in cls_idx:
-                    cls_idx.remove(cls)
-                    
         return torch.stack([
             (_outputs * weight).T
-            [cls_idx].mean(dim=0)
-            for cls_idx, weight in zip(cls_idxs, weights)
+            [node.new_to_old_classes[new_label]].mean(dim=0)
+            for new_label, weight in zip(range(node.num_classes), weights)
         ]).T
-
 
 class SoftTreeSupLoss(HardTreeSupLoss):
 
@@ -419,3 +403,98 @@ class SoftTreeSupMaskLoss(SoftTreeSupLoss):
             [cls_idx].mean(dim=0)
             for cls_idx, weight in zip(cls_idxs, weights)
         ]).T
+
+def CXE(predicted, target):
+    return -(target * torch.log(predicted)).sum(dim=1).mean()
+
+class ZSSoftTreeSupLoss(SoftTreeSupLoss):
+
+    def __init__(self, path_graph, path_wnids, classes,
+                 max_leaves_supervised=-1, min_leaves_supervised=-1,
+                 tree_supervision_weight=1., weighted_average=False,
+                 criterion=nn.CrossEntropyLoss()):
+        super().__init__(path_graph, path_wnids, classes,
+                         max_leaves_supervised, min_leaves_supervised,
+                         tree_supervision_weight, weighted_average, criterion)
+        self.zs_labels = list(range(64, 84))
+        self.pairings = {l: None for l in self.zs_labels}
+        self.pair_zs()
+        print(self.pairings)
+
+    def pair_zs(self):
+        for node in self.nodes:
+            children_ids = [node.new_to_old_classes[index_child][0] for index_child in range(len(node.children))]
+            intersection = [i for i in self.zs_labels if i in children_ids]
+            union = [i for i in children_ids if i not in self.zs_labels]
+            for label in intersection:
+                if len(union) > 0:
+                    print(f"pair {label} with {union}")
+                    self.pairings[label] = union[0]
+
+    def forward(self, outputs, targets):
+        smoothed_targets = self.smooth_one_hot(targets)
+        loss = self.criterion(outputs, targets)
+        bayesian_outputs = SoftTreeSupLoss.inference(
+            self.nodes, outputs, self.num_classes, self.weighted_average)
+        loss += CXE(bayesian_outputs, smoothed_targets) * self.tree_supervision_weight
+        return loss
+
+    @classmethod
+    def inference(cls, nodes, outputs, num_classes, weighted_average=False):
+        """
+        In theory, the loop over children below could be replaced with just a
+        few lines:
+
+            for index_child in range(len(node.children)):
+                old_indexes = node.new_to_old_classes[index_child]
+                class_probs[:,old_indexes] *= output[:,index_child][:,None]
+
+        However, we collect all indices first, so that only one tensor operation
+        is run.
+        """
+        class_probs = torch.ones((outputs.size(0), num_classes)).to(outputs.device)
+        for node in nodes:
+            output = cls.get_output_sub(outputs, node, weighted_average)
+            output = F.softmax(output)
+
+            old_indices, new_indices = [], []
+            for index_child in range(len(node.children)):
+                old = node.new_to_old_classes[index_child]
+                old_indices.extend(old)
+                new_indices.extend([index_child] * len(old))
+
+            assert len(set(old_indices)) == len(old_indices), (
+                'All old indices must be unique in order for this operation '
+                'to be correct.'
+            )
+            class_probs[:,old_indices] *= output[:,new_indices]
+        return class_probs
+
+    def smooth_one_hot(self, labels, smoothing=0.1):
+        """ create soft labels """
+        assert 0 <= smoothing < 1
+        label_shape = torch.Size((labels.size(0), self.num_classes))
+        confidence = 1.0 - smoothing
+
+        if smoothing == 0 or not self.pairings:
+            return torch.zeros_like(label_shape).scatter_(1, labels.data.unsqueeze(1), confidence)
+
+        with torch.no_grad():
+            true_dist = torch.zeros(size=label_shape, device=labels.device)
+            true_dist.scatter_(1, labels.data.unsqueeze(1), 1)
+        for vec in true_dist:
+            for zsl, seen in self.pairings.items():
+                if seen:
+                    if vec[seen] == 1:
+                        vec[seen] = confidence
+                        vec[zsl] = smoothing
+        # for zsl, seen in self.pairings.items():
+        #     if seen:
+        #         seen_selector = torch.zeros_like(labels.data.unsqueeze(1))
+        #         seen_selector[true_dist[:, seen] == 1] = seen
+        #         print(seen_selector)
+        #         zsl_selector = torch.zeros_like(labels.data.unsqueeze(1))
+        #         zsl_selector[true_dist[:, seen] == 1] = zsl
+        #         true_dist.scatter_(1, seen_selector, confidence)
+        #         true_dist.scatter_(1, zsl_selector, smoothing)
+        return true_dist
