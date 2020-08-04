02eab791e23e63733b8b883b6d0cecfbf56412a2
Weifan Chen <wfchen@scc4.bu.edu>
2020-08-04 00:36:08 -0400

    fix bug, and change execution flow

--- run_cifar10.py
+++ run_cifar10.py
@@ -14,53 +14,66 @@ def get_parser():
 	
 if __name__=='__main__':
 	args = get_parser().parse_args()
+	cmds = []
 
-	print('step 1 : train model. cmd to be submitted... in 5 second')
+	#print('step 1 : train model. cmd to be submitted... in 5 second')
 	arg_exclude_classes = ' '.join(args.exclude_classes)
 	arg_checkpoint_fname = '-'.join(['ckpt',args.model,'CIFAR10','exclude',*args.exclude_classes])
 	cmd = ' '.join(['python', 'main.py', '--model', args.model,'--dataset','CIFAR10ExcludeClasses', '--exclude-classes', arg_exclude_classes,'--checkpoint-fname', arg_checkpoint_fname])
-	print(cmd)
-	time.sleep(5)
-	os.system(cmd)
+	#print(cmd)
+	#time.sleep(5)
+	#os.system(cmd)
+	cmds.append(cmd)
 
-	print('step 2 : generate hierarchy. cmd to be sumitted ... in 5 second')
-	arg_exlude_labels = ' '.join(args.exclude_labels)
-	arg_indcued_checkpoint = './checkpoint/' + arg_checkpoint_fname + '.pth'
-	cmd = ' '.join(['python', 'generate_hierarchy.py', '--method', arg.method, '--dataset', 'CIFAR10', '--exclude-labels', arg_exclude_labels,
+	#print('step 2 : generate hierarchy. cmd to be sumitted ... in 5 second')
+	arg_exclude_labels = ' '.join(args.exclude_labels)
+	arg_induced_checkpoint = './checkpoint/' + arg_checkpoint_fname + '.pth'
+	cmd = ' '.join(['python', 'generate_hierarchy.py', '--method', args.method, '--dataset', 'CIFAR10', '--exclude-labels', arg_exclude_labels,
 			'--induced-checkpoint', arg_induced_checkpoint])
-	print(cmd)
-	time.sleep(5)
-	os.system(cmd)
+	#print(cmd)
+	#time.sleep(5)
+	#os.system(cmd)
+	cmds.append(cmd)
 
-	print('step 3 : fine tune. cmd to be submitted in 5 sec')
+	#print('step 3 : fine tune. cmd to be submitted in 5 sec')
 	arg_path_graph = './data/CIFAR10/' + '-'.join(['graph','induced','exclude',*args.exclude_classes]) + '.json'
 	cmd = ' '.join(['python','main.py','--model',args.model,'--dataset','CIFAR10ExcludeClasses','--exclude-classes',arg_exclude_classes,'--resume','--path-resume',arg_induced_checkpoint,'--loss','SoftTreeSupLoss','--path-graph',arg_path_graph, '--freeze-conv','--analysis', 'SoftEmbeddedDecisionRules'])
-	print(cmd)
-	time.sleep(5)
-	os.system(cmd)
+	#print(cmd)
+	#time.sleep(5)
+	#os.system(cmd)
+	cmds.append(cmd)
 
-	print('step 4 : add weight vector. cmd to be sub in 5 sec')
+	#print('step 4 : add weight vector. cmd to be sub in 5 sec')
 	arg_new_checkpoint_fname = '-'.join(['ckpt',args.model,'CIFAR10','exclude',*args.exclude_classes,'weighted'])
 	cmd = ' '.join(['python', 'add_zeroshot_vec.py', '--model', args.model, '--resume', '--path-resume', arg_induced_checkpoint, '--new-classes', arg_exclude_classes, '--num-samples', args.num_samples, '--checkpoint-fname', arg_new_checkpoint_fname])
-	print(cmd)
-	time.sleep(5)
-	os.system(cmd)
+	#print(cmd)
+	#time.sleep(5)
+	#os.system(cmd)
+	cmds.append(cmd)
 
-	print('step 5 : reform hierarchy, in 5 sec')
+	#print('step 5 : reform hierarchy, in 5 sec')
 	arg_new_induced_checkpoint = './checkpoint/' + arg_new_checkpoint_fname + '.pth'
 	cmd = ' '.join(['python', 'generate_hierarchy.py', '--method', args.method, '--dataset', 'CIFAR10', '--ignore-labels', arg_exclude_labels, '--induced-checkpoint', arg_new_induced_checkpoint])
-	print(cmd)
-	time.sleep(5)
-	os.system(cmd)
+	#print(cmd)
+	#time.sleep(5)
+	#os.system(cmd)
+	cmds.append(cmd)
 
-	print('step 6 : evaluate, in 5 sec')
+	#print('step 6 : evaluate, in 5 sec')
 	arg_new_path_graph = './data/CIFAR10/' + '-'.join(['graph','induced','exclude',*args.exclude_classes,'weighted']) + '.json'
 	cmd = ' '.join(['python','main.py','--model', args.model,'--dataset', 'CIFAR10', '--resume', '--eval', '--path-resume', arg_new_induced_checkpoint, '--path-graph', arg_new_path_graph, '--analysis', 'SoftEmbeddedDecisionRules'])
-	print(cmd)
-	time.sleep(5)
-	os.system(cmd)
+	#print(cmd)
+	#time.sleep(5)
+	#os.system(cmd)
+	cmds.append(cmd)
 
-
+	print()
+	for cmd in cmds: print(cmd)
+	option = input('Submit the above commands? y/n ')
+	if option=='y':
+		for cmd in cmds: os.system(cmd)
+	else:
+		pass
 
 
 """
